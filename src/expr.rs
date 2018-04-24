2be11d7b2871a038cbad64a72a8d3e039ee79d76
Rohan <rohan@exodar.local>
2018-04-23 21:31:28 -0700

    some expressions

---
+++ src/expr.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let _x = 10;
+    let y = {
+        let z = 200;
+        z+20
+    };
+    if y < 100 {
+        println!("y is less than 100");
+    } else if y < 200 {
+        println!("y is less than 200");
+    } else {
+        println!("y is not less than 200");
+    }
+}
